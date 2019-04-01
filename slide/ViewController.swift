6eb98e3b6e8344f39ecfff064aa6c8ce06666edf
USER <user@user-no-macbook-pro.local>
2019-04-01 19:14:31 +0900

    Initial Commit

---
+++ slide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slide
+//
+//  Created by USER on 2019/04/01.
+//  Copyright © 2019 sf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
