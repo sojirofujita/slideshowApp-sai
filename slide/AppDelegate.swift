70c67dc0e456902312c44452336d81a6b65b57a0
USER <user@user-no-macbook-pro.local>
2019-04-02 18:21:31 +0900

    次へボタン+戻るボタンで画像送り成功

--- slide/AppDelegate.swift
+++ slide/AppDelegate.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 sf. All rights reserved.
 //
 
-import UIKit
+ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
--- slide/ViewController.swift
+++ slide/ViewController.swift
@@ -6,42 +6,62 @@
 //  Copyright © 2019 sf. All rights reserved.
 //
 
+//UIKitのインポート
 import UIKit
 
+// サブクラス：スーパークラス
 class ViewController: UIViewController {
 
+    
+    //Imageviewをoutlet接続。後のIBactionでImageViewの画像をいじるため恐らく必要。
     @IBOutlet weak var imageView: UIImageView!
     
-    @IBAction func nextButton(_ sender: Any) {
-        dispImageNo -= 1
+    
+    //次へボタンをAction接続。ImageViewを触るため必要。
+    @IBAction func nextbutton(_ sender: Any) {
+        //表示した画像の番号を１増やす
+        dispImageNo += 1
+        
         displayImage()
     }
-    
+
     @IBAction func prevbutton(_ sender: Any) {
-        dispImageNo += 1
+        //表示した画像の番号を１減らす
+        dispImageNo -= 1
+        
         displayImage()
         
     }
+    
+    //変数名dispImageNoを０で宣言
     var dispImageNo = 0
+    
+    //関数displayImageのメソッド記述
     func displayImage(){
+        //定数imageNameArrayを配列として纏めておく
         let imageNameArray = [
-        "firstimage","secondimage","thirdimage"]
+        "image1.jpg","image2.jpg","image3.jpeg"]
+        
+        //dispImageNoが０より小さい時に値を２にする。
         if dispImageNo < 0 {
             dispImageNo = 2
         }
+        
+        //dispImageNoが2より大きい時に値を０とする。
         if dispImageNo > 2{
             dispImageNo = 0
         }
+        //定数名nameの宣言。nameへimageNameArray配列[no]を代入
         let name = imageNameArray[dispImageNo]
+        //????????
         let image = UIImage(named: name)
+        //????????
         imageView.image = image
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let image = UIImage(named: "image1")
-        imageView.image = image
     }
 
 
